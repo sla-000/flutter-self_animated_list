a9e7400765cadc6de232db5b29fafe5e806c4b3c
Vyacheslav Ryabinin <ryabinin.v.v@gmail.com>
2022-07-17 17:21:58 +0300

    Add findDifferenceIndex swap tests

--- test/iterate_search_change_test.dart
+++ test/iterate_search_change_test.dart
@@ -48,6 +48,29 @@ void main() {
         expect(findDifferenceIndex<int>(initial: initial, target: target), 2);
       });
     });
+
+    group('findDifferenceIndex, swap', () {
+      test('findDifferenceIndex, swap at start', () {
+        final List<int> initial = <int>[1, 2, 3];
+        const List<int> target = <int>[2, 1, 3];
+
+        expect(findDifferenceIndex<int>(initial: initial, target: target), 0);
+      });
+
+      test('findDifferenceIndex, swap at middle', () {
+        final List<int> initial = <int>[1, 2, 3, 4, 5];
+        const List<int> target = <int>[1, 4, 3, 2, 5];
+
+        expect(findDifferenceIndex<int>(initial: initial, target: target), 1);
+      });
+
+      test('findDifferenceIndex, remove at end', () {
+        final List<int> initial = <int>[1, 2, 3];
+        const List<int> target = <int>[1, 3, 2];
+
+        expect(findDifferenceIndex<int>(initial: initial, target: target), 1);
+      });
+    });
   });
 
   group('iterateSearchChanges, add', () {
