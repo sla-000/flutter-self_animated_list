0d8f1bd70f8d2e81b940134fba0a0164e53ee813
Vyacheslav Ryabinin <ryabinin.v.v@gmail.com>
2022-07-30 14:44:05 +0300

    Refactor

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,11 +1,10 @@
 import 'package:example/logic/list_cubit.dart';
-import 'package:example/model/item_model.dart';
 import 'package:example/ui/add_rem_value.dart';
-import 'package:example/ui/item_tile.dart';
+import 'package:example/ui/horizontal_tiles.dart';
 import 'package:example/ui/swap_value.dart';
+import 'package:example/ui/vertical_tiles.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:self_animated_list/self_animated_list.dart';
 
 void main() => runApp(const MyApp());
 
@@ -34,17 +33,10 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   late final ListCubit _horizontalCubit = ListCubit(5);
   late final ListCubit _verticalCubit = ListCubit(5);
+
   int _toRemove = 1;
   int _toAdd = 1;
   int _toSwap = 0;
-
-  @override
-  void dispose() {
-    _horizontalCubit.close();
-    _verticalCubit.close();
-
-    super.dispose();
-  }
 
   @override
   Widget build(BuildContext context) {
@@ -55,13 +47,19 @@ class _MyHomePageState extends State<MyHomePage> {
       body: Column(
         crossAxisAlignment: CrossAxisAlignment.stretch,
         children: <Widget>[
-          _horizontalTiles(),
+          BlocProvider<ListCubit>(
+            create: (context) => _horizontalCubit,
+            child: const HorizontalTiles(),
+          ),
           const Divider(),
           Expanded(
             child: Row(
               crossAxisAlignment: CrossAxisAlignment.start,
               children: <Widget>[
-                _verticalTiles(),
+                BlocProvider<ListCubit>(
+                  create: (context) => _verticalCubit,
+                  child: const VerticalTiles(),
+                ),
                 Expanded(
                   child: Center(
                     child: _controls(),
@@ -119,79 +117,4 @@ class _MyHomePageState extends State<MyHomePage> {
       ),
     );
   }
-
-  Widget _verticalTiles() {
-    return SizedBox(
-      width: kTileWidth,
-      child: BlocBuilder<ListCubit, List<ItemModel>>(
-        bloc: _verticalCubit,
-        builder: (BuildContext context, List<ItemModel> state) {
-          return SelfAnimatedList<ItemModel>(
-            physics: const BouncingScrollPhysics(),
-            scrollDirection: Axis.vertical,
-            addDuration: const Duration(milliseconds: 2000),
-            removeDuration: const Duration(milliseconds: 3000),
-            initialItemCount: _verticalCubit.state.length,
-            data: _verticalCubit.state,
-            itemBuilder: _buildItem,
-            addBuilder: (Animation<double> animation, Widget child) => defaultAddBuilder(
-              animation: animation,
-              child: child,
-            ),
-            removeBuilder: _bounceRemoveBuilder,
-          );
-        },
-      ),
-    );
-  }
-
-  Widget _horizontalTiles() {
-    return SizedBox(
-      height: kTileHeight,
-      child: BlocBuilder<ListCubit, List<ItemModel>>(
-        bloc: _horizontalCubit,
-        builder: (BuildContext context, List<ItemModel> state) {
-          return SelfAnimatedList<ItemModel>(
-            physics: const BouncingScrollPhysics(),
-            scrollDirection: Axis.horizontal,
-            addDuration: const Duration(milliseconds: 1500),
-            removeDuration: const Duration(milliseconds: 1500),
-            initialItemCount: _horizontalCubit.state.length,
-            data: _horizontalCubit.state,
-            itemBuilder: _buildItem,
-            addBuilder: (Animation<double> animation, Widget child) => defaultAddBuilder(
-              animation: animation,
-              axis: Axis.horizontal,
-              child: child,
-            ),
-            removeBuilder: (Animation<double> animation, Widget child) => defaultRemoveBuilder(
-              animation: animation,
-              axis: Axis.horizontal,
-              child: child,
-            ),
-          );
-        },
-      ),
-    );
-  }
-}
-
-Widget _buildItem(_, int index, ItemModel item) => ItemTile(
-      key: ValueKey(item.value),
-      color: item.color,
-      index: index,
-    );
-
-SizeTransition _bounceRemoveBuilder(Animation<double> animation, Widget child) {
-  final sizeAnimation = CurvedAnimation(parent: animation, curve: Curves.bounceIn);
-  final opacityAnimation = animation;
-
-  return SizeTransition(
-    sizeFactor: sizeAnimation,
-    axis: Axis.vertical,
-    child: FadeTransition(
-      opacity: opacityAnimation,
-      child: child,
-    ),
-  );
 }
